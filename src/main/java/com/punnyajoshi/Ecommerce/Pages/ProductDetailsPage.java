1db7d9ed5b10d298bace76c0caf247761b36ed22
punnyajoshi <punnyajoshi@gmail.com>
2024-05-09 17:29:46 +0530

    add to cart functionality

--- src/main/java/com/punnyajoshi/Ecommerce/Pages/ProductDetailsPage.java
+++ src/main/java/com/punnyajoshi/Ecommerce/Pages/ProductDetailsPage.java
@@ -2,30 +2,44 @@ package com.punnyajoshi.Ecommerce.Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ProductDetailsPage {
     private WebDriver driver;
-    private By productDetailsLocator = By.cssSelector("div.product-details");
     private By productNameLocator = By.cssSelector("h1.product-name");
-    private By addToCartButtonLocator = By.cssSelector("button.add-to-cart");
+    private By productAvailabilityLocator = By.cssSelector("p.product-availability");
 
     public ProductDetailsPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public boolean isProductDetailsLoaded() {
-        WebDriverWait wait = new WebDriverWait(driver, 100);
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(productDetailsLocator))!= null;
+    public ProductDetailsPage(WebDriver driver, By productNameLocator, By productAvailabilityLocator) {
+        this.driver = driver;
+        this.productNameLocator = productNameLocator;
+        this.productAvailabilityLocator = productAvailabilityLocator;
     }
 
     public String getProductName() {
-        return driver.findElement(productNameLocator).getText();
+        WebElement productNameElement = driver.findElement(productNameLocator);
+        return productNameElement.getText();
+    }
+
+    public boolean isProductAvailable() {
+        WebElement productAvailabilityElement = driver.findElement(productAvailabilityLocator);
+        return productAvailabilityElement.getText().equals("In Stock");
+    }
+
+    public boolean isProductDetailsLoaded() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(productNameLocator)).isDisplayed();
     }
 
     public void addToCart() {
-        driver.findElement(addToCartButtonLocator).click();
+        driver.findElement(By.cssSelector("button.add-to-cart")).click();
     }
+
+
 
 }
