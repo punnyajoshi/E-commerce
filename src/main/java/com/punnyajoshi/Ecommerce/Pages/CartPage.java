814a659796cbe1656ed5eb52e83f41438f651599
punnyajoshi <punnyajoshi@gmail.com>
2024-05-11 23:12:30 +0530

    validating cart contents

--- src/main/java/com/punnyajoshi/Ecommerce/Pages/CartPage.java
+++ src/main/java/com/punnyajoshi/Ecommerce/Pages/CartPage.java
@@ -6,12 +6,21 @@ public class CartPage {
     private WebDriver driver;
     private String cartUrl;
 
-    public CartPage(WebDriver driver, String cartUrl) {
+    public CartPage(WebDriver driver) {
         this.driver = driver;
         this.cartUrl = cartUrl;
     }
 
     public void navigateToCartPage() {
         driver.get(cartUrl);
+    }
+
+    public void addProductToCart(String s) {
+        driver.getCurrentUrl();
+    }
+
+    public String productsAreDisplayedCorrectly() {
+        driver.getCurrentUrl();
+        return null;
     }
 }
--- src/main/java/com/punnyajoshi/Ecommerce/ShoppingCartApp.java
+++ src/main/java/com/punnyajoshi/Ecommerce/ShoppingCartApp.java
@@ -2,31 +2,40 @@ package com.punnyajoshi.Ecommerce;
 
 import com.punnyajoshi.Ecommerce.Pages.CartPage;
 import com.punnyajoshi.Ecommerce.Pages.Product;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ShoppingCartApp{
 
-public static void main(String[] args) {
-    // Create cart instance
-    Cart cart = new Cart();
+    public static void main(String[] args) {
+        // Initialize the WebDriver and cartUrl here
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://web-playground.ultralesson.com");
 
-    // Adding products to the cart (example products)
-    Product product = new Product("Sweater", "L", 1, 49.99);
-    cart.addProduct(product);
+        driver.findElement(By.xpath("//a[@href='/cart']")).click();
 
-    // Navigate to the cart page
-    CartPage cartPage = new CartPage(driver, cartUrl);
-    cartPage.navigateToCartPage();
+        // Create cart instance
+        Cart cart = new Cart();
 
-    // Logic to display cart contents
-    for (Product p : cart.getProducts()) {
-        System.out.println("Name: " + p.getName());
-        System.out.println("Size: " + p.getSize());
-        System.out.println("Price: " + p.getPrice());
-        System.out.println("Quantity: " + p.getQuantity());
-        System.out.println("--------------------");
+        // Adding products to the cart (example products)
+        Product product = new Product("Sweater", "L", 1, 49.99);
+        cart.addProduct(product);
+
+
+        // Logic to display cart contents
+        for (Product p : cart.getProducts()) {
+            System.out.println("Name: " + p.getName());
+            System.out.println("Size: " + p.getSize());
+            System.out.println("Price: " + p.getPrice());
+            System.out.println("Quantity: " + p.getQuantity());
+            System.out.println("--------------------");
+        }
+
+        // Compute and show total price here
+        System.out.println("Total price: $" + cart.getTotalPrice());
+
+        // Close the WebDriver
+        driver.quit();
     }
-
-    // Compute and show total price here
-    System.out.println("Total price: $" + cart.getTotalPrice());
-}
 }
